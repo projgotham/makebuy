77a238976f9449fbf747ed35864f11452e3de043
philoz3231 <soaring3231@gmail.com>
2016-03-21 21:16:35 +0900

    guide layout modified

---
+++ content/service.php
@@ -0,0 +1,26 @@
+<style>
+    @media only screen and (min-width: 768px) {
+        section.content > .image { width:40%; height:auto; }
+        section.content > .title { padding-left:45%;}
+    }
+    @media only screen and (max-width: 767px) {
+        section.content > .image { width:100%;height:auto; max-width:100%; }
+        section.content > .image img { max-width:100%; max-height:999px;}
+    }
+
+</style>
+<section class="content" id="service">
+    <div class='image'><img src='./images/icons/service.jpg' align='absmiddle' /></div>
+    <div class='title'>
+        <h2>
+            Makebuy는 어떤 서비스인가요?
+            <div class='border'><span></span></div>
+        </h2>
+        <h3>
+            Makebuy는 앱을 만들고 싶은 사람(클라이언트)와 앱을 만들어줄 수 있는 사람(프리랜서)를 연결해 주는 중개서비스입니다.<br>
+            Makebuy와 함께라면 이제 내가 원하던 앱을 ‘make’하고 ‘buy’할 수 있습니다.
+        </h3>
+    </div>
+</section>
+</div>
+</div>
