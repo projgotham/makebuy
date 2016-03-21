5797d49dfaba367b498dad1dad59ea06638813dd
philoz3231 <soaring3231@gmail.com>
2016-03-21 21:28:45 +0900

    company layout modified

--- content/company.php
+++ content/company.php
@@ -1,10 +1,10 @@
 <style>
+	section#purpose .title { padding-left:0; padding-right:250px;text-align:right }
+	section#purpose .image { float:right; }
 	section#contact .title h2 { margin-bottom:40px; }
 	section#contact .title h3 { padding:0; line-height:100%; text-transform: none;}
 
 	@media only screen and (min-width: 768px) {
-		section#purpose .title { padding-left:0; padding-right:250px;text-align:right }
-		section#purpose .image { float:right; }
 		section#contact .image { width:400px; height:300px;}
 		section#contact .title { padding-left:450px;;}
 	}
@@ -16,26 +16,24 @@
 		#price .tbl_type td { padding-left:20%; }
 		section#contact .image { width:100%;height:auto; max-width:400px;max-height:300px;}
 		section#contact .image iframe { width:100%;height:auto; }
-		section .title h3 { font-size:14px;}
-		section .title h3 * { font-size:14px;}
 	}
 </style>
-		<section class="content" id="vision">
-			<div class='image'>
-				<img src='./images/content/vision.png' align='absmiddle' />
-			</div>
-			<div class='title'>
-				<h2>
-					비전
-					<div class='border'><span></span></div>
-				</h2>
-				<h3>사람과 사람 사이의 진심을 전달하고, <br>최고의 성과가 나오도록 돕는다.</h3>
-			</div>
-		</section>
+<section class="content" id="vision">
+	<div class='image'>
+		<img src='./images/content/vision.png' align='absmiddle' />
 	</div>
+	<div class='title'>
+		<h2>
+			비전
+			<div class='border'><span></span></div>
+		</h2>
+		<h3>사람과 사람 사이의 진심을 전달하고, <br>최고의 성과가 나오도록 돕는다.</h3>
+	</div>
+</section>
+</div>
 </div>
 <div class='sec2'>
-    <div class="container">
+	<div class="container">
 		<section class="content" id="purpose">
 			<div class='image'>
 				<img src='./images/content/purpose.png' align='absmiddle' />
@@ -46,20 +44,15 @@
 					<div class='border'><span></span></div>
 				</h2>
 				<h3>
-					Makebuy는 기술에 대한 지식이 부족하여
-					<li></li>앱 서비스를 만들기 어려워 하는 사람들과
-					<li></li>적합한 재능이 있는 프리랜서를 이어줍니다. <br>
-					<li></li>Makebuy는 서로 간의 진심이 이어질 때 
-					<li></li>가장 좋은 결과가 나오며
-					<li></li>이렇게 탄생한 서비스들이  
-					<li></li>더 나은 세상을 만들어 갈 것이라고 믿습니다.
+					Makebuy는 기술에 대한 지식이 부족하여 앱 서비스를 만들기 어려워 하는 사람들과  적합한 재능이 있는 프리랜서를 이어줍니다. <br>
+					Makebuy는 서로 간의 진심이 이어질 때 가장 좋은 결과가 나오며 이렇게 탄생한 서비스들이  더 나은 세상을 만들어 갈 것이라고 믿습니다.
 				</h3>
 			</div>
 		</section>
 	</div>
 </div>
 <div class='sec1'>
-    <div class="container">
+	<div class="container">
 		<section class="content" id="contact">
 			<div class='image'>
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1583.1304988872641!2d127.12066161169037!3d37.47816688483605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca5f6606eff87%3A0x671eae5c7fc3ab6b!2zKOyjvCnqsIDrk6DtjIzsnbTruIztiLQ!5e0!3m2!1sko!2s!4v1458200888539" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
