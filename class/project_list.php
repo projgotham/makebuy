e486807603996cc8ad74c7c1dd310823eb4ea0c0
philoz3231 <soaring3231@gmail.com>
2016-03-25 01:29:47 +0900

    apply sorting projects with tabs

--- class/project_list.php
+++ class/project_list.php
@@ -64,7 +64,7 @@ class project_list {
 
         $db = new db();
         $db->connect();
-        $sql = "SELECT * FROM project_tb WHERE proj_state='recruit' or proj_state='finish'";
+        $sql = "SELECT * FROM project_tb WHERE proj_state='recruit' or proj_state='finish' ORDER BY proj_upload DESC";
         $rows = $db->select($sql);
 
         foreach($rows as $row) {
--- content/search-projects.php
+++ content/search-projects.php
@@ -90,21 +90,22 @@ foreach($project_list as $project){
 <section class="section-search-result js--section-search-result">
 	<div class='title'>
 		<h2 style='font-size:20px;'>
-			총 <b class="color1"><?php echo count($project_list)?></b>건의 검색 결과를 찾았습니다.
+			총 <b class="color1"><?php echo count($project_list)?></b>건의 프로젝트가 등록되었습니다.
 			<div class='border'><span></span></div>
 		</h2>
 	</div>
-    <div class="row">
+    <div class="tabs">
 		<div class='tab_list'>
 			<ul>
-				<li><a href="#">최신 순</a></li>
-				<li><a href="#">내 기술에 맞게</a></li>
-				<li><a href="#">마감임박 순</a></li>
-				<li><a href="#">금액 순</a></li>
+				<li><a href="#tab1-recent">최신 순</a></li>
+				<li><a href="#tab2-high">높은 금액 순</a></li>
+				<li><a href="#tab3-low">낮은 금액 순</a></li>
+				<li><a href="#tab4-deadline">마감임박 순</a></li>
 			</ul>
 		</div>
+
         <div class="tab-content">
-			<section>
+			<section id="tab1-recent" class="tab active">
 				<div class="tbl_type">
 					<table>
 						<col />
@@ -122,6 +123,15 @@ foreach($project_list as $project){
 							</tr>
 						</thead>
 						<?php
+						//sort project by upload time
+						function compare_upload($a, $b){
+							if ($a->getProjUploadDate() == $b->getProjUploadDate()){
+								return 0;
+							}
+							return ($a->getProjUploadDate() > $b->getProjUploadDate()) ? -1 : 1;
+						}
+						usort($project_list, 'compare_upload');
+
 						foreach($project_list as $project ){
 							$project_key = $project->getProjKey();
 							$project_state = $project->getProjState();
@@ -175,21 +185,265 @@ foreach($project_list as $project){
 							}
 						}
 						?>
-						<!-- <tbody class="disable">
-							<tr>
-								<td class="subject"><span class="t-button active"><span>마감</span></span>&nbsp;<a href="./sub.php?page=project-intro"><b>프로젝트 타이틀</b></a>&nbsp;(Android, iOS, HTML 필요)</p></td>
-								<td data-title="예상금액"></td>
-								<td data-title="지원마감">6일 전</td>
-								<td data-title="예상기한">10일</td>
-								<td data-title="지원자">52명</td>
-							</tr>
-						</tbody>
-						-->
-
 					 </table>
 				</div>
 				<div class="col span-5-of-7 result-detail result-detail-spec"></div>
 			</section>
+
+			<section id="tab2-high" class="tab">
+				<div class="tbl_type">
+					<table>
+						<col />
+						<col width="15%" />
+						<col width="15%" />
+						<col width="12%" />
+						<col width="12%" />
+						<thead>
+						<tr>
+							<th>프로젝트명(내용)</th>
+							<th>예상금액</th>
+							<th>지원마감</th>
+							<th>예상기한</th>
+							<th>지원자</th>
+						</tr>
+						</thead>
+						<?php
+						//sort project by high price
+						function compare_high($a, $b){
+							if ($a->getProjExpPrice() == $b->getProjExpPrice()){
+								return 0;
+							}
+							return ($a->getProjExpPrice() > $b->getProjExpPrice()) ? -1 : 1;
+						}
+						usort($project_list, 'compare_high');
+						foreach($project_list as $project ){
+							$project_key = $project->getProjKey();
+							$project_state = $project->getProjState();
+							$project_name = $project->getProjName();
+							$projExpPrice = $project->getProjExpPrice();
+							$projDeadLine = $project->getProjDeadLine();
+							$projExpPeriod = $project->getProjExpPeriod();
+							$project_participant_list = $project->getProjParticipants();
+							$project_type_list = $project->getProjTypes();
+
+
+							if($project_state == 'finish'){
+								echo "<tbody class='disable'>";
+								echo "<tr>";
+								echo "<td class=\"subject\"><span class=\"t-button active\"><span>마감</span></span>&nbsp;<a href=\"./sub.php?page=project-intro&projid=$project_key\"><b>$project_name</b></a>&nbsp;";
+								if($project_type_list != null){
+									echo "(";
+									foreach($project_type_list as $project_type){
+										$type = $project_type->getProjType();
+										echo "$type, ";
+									}
+									echo "필요)";
+								}
+								echo "</p></td>";
+								echo '<td data-title=\"예상금액\">'.number_format($projExpPrice).'</td>';
+								echo "<td data-title=\"지원마감\">$projDeadLine 전</td>";
+								echo "<td data-title=\"예상기한\">$projExpPeriod 일</td>";
+								echo '<td data-title=\"지원자\">'.count($project_participant_list).' 명</td>';
+								echo "</tr>";
+								echo "</tbody>";
+							}
+							else{
+								echo "<tbody>";
+								echo "<tr>";
+								echo "<td class=\"subject\"><span class=\"t-button color\"><span>모집중</span></span>&nbsp;<a href=\"./sub.php?page=project-intro&projid=$project_key\"><b>$project_name</b></a>&nbsp;";
+								if($project_type_list != null){
+									echo "(";
+									foreach($project_type_list as $project_type){
+										$type = $project_type->getProjType();
+										echo "$type, ";
+									}
+									echo "필요)";
+								}
+								echo "</p></td>";
+								echo '<td data-title=\"예상금액\">'.number_format($projExpPrice).'</td>';
+								echo "<td data-title=\"지원마감\">$projDeadLine 전</td>";
+								echo "<td data-title=\"예상기한\">$projExpPeriod 일</td>";
+								echo '<td data-title=\"지원자\">'.count($project_participant_list).' 명</td>';
+								echo "</tr>";
+								echo "</tbody>";
+							}
+						}
+						?>
+					</table>
+				</div>
+				<div class="col span-5-of-7 result-detail result-detail-spec"></div>
+			</section>
+
+			<section id="tab3-low" class="tab">
+				<div class="tbl_type">
+					<table>
+						<col />
+						<col width="15%" />
+						<col width="15%" />
+						<col width="12%" />
+						<col width="12%" />
+						<thead>
+						<tr>
+							<th>프로젝트명(내용)</th>
+							<th>예상금액</th>
+							<th>지원마감</th>
+							<th>예상기한</th>
+							<th>지원자</th>
+						</tr>
+						</thead>
+						<?php
+						//sort project by low
+						function compare_low($a, $b){
+							if ($a->getProjExpPrice() == $b->getProjExpPrice()){
+								return 0;
+							}
+							return ($a->getProjExpPrice() < $b->getProjExpPrice()) ? -1 : 1;
+						}
+						usort($project_list, 'compare_low');
+
+						foreach($project_list as $project ){
+							$project_key = $project->getProjKey();
+							$project_state = $project->getProjState();
+							$project_name = $project->getProjName();
+							$projExpPrice = $project->getProjExpPrice();
+							$projDeadLine = $project->getProjDeadLine();
+							$projExpPeriod = $project->getProjExpPeriod();
+							$project_participant_list = $project->getProjParticipants();
+							$project_type_list = $project->getProjTypes();
+
+
+							if($project_state == 'finish'){
+								echo "<tbody class='disable'>";
+								echo "<tr>";
+								echo "<td class=\"subject\"><span class=\"t-button active\"><span>마감</span></span>&nbsp;<a href=\"./sub.php?page=project-intro&projid=$project_key\"><b>$project_name</b></a>&nbsp;";
+								if($project_type_list != null){
+									echo "(";
+									foreach($project_type_list as $project_type){
+										$type = $project_type->getProjType();
+										echo "$type, ";
+									}
+									echo "필요)";
+								}
+								echo "</p></td>";
+								echo '<td data-title=\"예상금액\">'.number_format($projExpPrice).'</td>';
+								echo "<td data-title=\"지원마감\">$projDeadLine 전</td>";
+								echo "<td data-title=\"예상기한\">$projExpPeriod 일</td>";
+								echo '<td data-title=\"지원자\">'.count($project_participant_list).' 명</td>';
+								echo "</tr>";
+								echo "</tbody>";
+							}
+							else{
+								echo "<tbody>";
+								echo "<tr>";
+								echo "<td class=\"subject\"><span class=\"t-button color\"><span>모집중</span></span>&nbsp;<a href=\"./sub.php?page=project-intro&projid=$project_key\"><b>$project_name</b></a>&nbsp;";
+								if($project_type_list != null){
+									echo "(";
+									foreach($project_type_list as $project_type){
+										$type = $project_type->getProjType();
+										echo "$type, ";
+									}
+									echo "필요)";
+								}
+								echo "</p></td>";
+								echo '<td data-title=\"예상금액\">'.number_format($projExpPrice).'</td>';
+								echo "<td data-title=\"지원마감\">$projDeadLine 전</td>";
+								echo "<td data-title=\"예상기한\">$projExpPeriod 일</td>";
+								echo '<td data-title=\"지원자\">'.count($project_participant_list).' 명</td>';
+								echo "</tr>";
+								echo "</tbody>";
+							}
+						}
+						?>
+					</table>
+				</div>
+				<div class="col span-5-of-7 result-detail result-detail-spec"></div>
+			</section>
+
+			<section id="tab4-deadline" class="tab">
+				<div class="tbl_type">
+					<table>
+						<col />
+						<col width="15%" />
+						<col width="15%" />
+						<col width="12%" />
+						<col width="12%" />
+						<thead>
+						<tr>
+							<th>프로젝트명(내용)</th>
+							<th>예상금액</th>
+							<th>지원마감</th>
+							<th>예상기한</th>
+							<th>지원자</th>
+						</tr>
+						</thead>
+						<?php
+						//sort project by deadline
+						function compare_deadline($a, $b){
+							if ($a->getProjDeadLine() == $b->getProjDeadLine()){
+								return 0;
+							}
+							return ($a->getProjDeadLine() > $b->getProjDeadLine()) ? -1 : 1;
+						}
+						usort($project_list, 'compare_deadline');
+
+						foreach($project_list as $project ){
+							$project_key = $project->getProjKey();
+							$project_state = $project->getProjState();
+							$project_name = $project->getProjName();
+							$projExpPrice = $project->getProjExpPrice();
+							$projDeadLine = $project->getProjDeadLine();
+							$projExpPeriod = $project->getProjExpPeriod();
+							$project_participant_list = $project->getProjParticipants();
+							$project_type_list = $project->getProjTypes();
+
+
+							if($project_state == 'finish'){
+								echo "<tbody class='disable'>";
+								echo "<tr>";
+								echo "<td class=\"subject\"><span class=\"t-button active\"><span>마감</span></span>&nbsp;<a href=\"./sub.php?page=project-intro&projid=$project_key\"><b>$project_name</b></a>&nbsp;";
+								if($project_type_list != null){
+									echo "(";
+									foreach($project_type_list as $project_type){
+										$type = $project_type->getProjType();
+										echo "$type, ";
+									}
+									echo "필요)";
+								}
+								echo "</p></td>";
+								echo '<td data-title=\"예상금액\">'.number_format($projExpPrice).'</td>';
+								echo "<td data-title=\"지원마감\">$projDeadLine 전</td>";
+								echo "<td data-title=\"예상기한\">$projExpPeriod 일</td>";
+								echo '<td data-title=\"지원자\">'.count($project_participant_list).' 명</td>';
+								echo "</tr>";
+								echo "</tbody>";
+							}
+							else{
+								echo "<tbody>";
+								echo "<tr>";
+								echo "<td class=\"subject\"><span class=\"t-button color\"><span>모집중</span></span>&nbsp;<a href=\"./sub.php?page=project-intro&projid=$project_key\"><b>$project_name</b></a>&nbsp;";
+								if($project_type_list != null){
+									echo "(";
+									foreach($project_type_list as $project_type){
+										$type = $project_type->getProjType();
+										echo "$type, ";
+									}
+									echo "필요)";
+								}
+								echo "</p></td>";
+								echo '<td data-title=\"예상금액\">'.number_format($projExpPrice).'</td>';
+								echo "<td data-title=\"지원마감\">$projDeadLine 전</td>";
+								echo "<td data-title=\"예상기한\">$projExpPeriod 일</td>";
+								echo '<td data-title=\"지원자\">'.count($project_participant_list).' 명</td>';
+								echo "</tr>";
+								echo "</tbody>";
+							}
+						}
+						?>
+					</table>
+				</div>
+				<div class="col span-5-of-7 result-detail result-detail-spec"></div>
+			</section>
+
 		</div>
     </div>
 </section>
